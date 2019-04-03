b9dd38cc570557e3b8a22cddbfe3eee9d121839e
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2019-04-04 04:07:17 +0500

    Add load results function

--- Main.cs
+++ Main.cs
@@ -17,13 +17,35 @@ namespace DARM_sharp
         {
             InitializeComponent();
 
-            using (var conn =
-                new NpgsqlConnection(
-                    "Server=db.mirvoda.com; Port=5454; User Id=developer; Password=rtfP@ssw0rd; Database=darm"))
-            {
-                conn.Open();
-            }
+            var conn = new NpgsqlConnection("Server=db.mirvoda.com; Port=5454; User Id=developer; Password=rtfP@ssw0rd; Database=darm");
 
+            LoadResultsToView("SELECT * FROM movies LIMIT 100", conn);
+        }
+
+        public void LoadResultsToView(string query, NpgsqlConnection conn)
+        {
+            conn.Open();
+            var cmd = new NpgsqlCommand(query, conn);
+
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                try
+                {
+                    var source_id = reader.GetInt32(0);
+                    var source_name = reader.GetString(1);
+                    var yearInt = 0;
+                    int.TryParse(reader.GetInt16(2).ToString(), out yearInt);
+
+                    searchResults.Rows.Add(reader.GetInt32(0), reader.GetString(1), yearInt.ToString());
+                }
+                catch
+                {
+                    // Error handler
+                }
+            }
+            reader.Close();
         }
     }
 }
